d7aff80aa95c9245d3f14c5d169a7af9574c51fa
adam <adam.ouhrabka@gmail.com>
2017-12-07 11:22:35 +0100

    after lesson 2

---
+++ src/model/curve.java
@@ -0,0 +1,22 @@
+package model;
+
+import transforms.Cubic;
+import transforms.Point3D;
+
+public class curve {
+	public Curve() {
+		int numbeOf = 10;
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Point3D p1 = new Point3D(-1,-1,-1);
+		Cubic cubic = new Cubic(Cubic.BEZIER,p1,p2,p3,p4);
+		for() {
+			indexBuffer.add();
+			indexBuffer.add();
+		}
+		parts.add(new Part(Part.Type.Lines,0,numberof))
+	}
+}
